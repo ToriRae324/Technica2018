4537c5f73aaab447338ff35eff0d58a82144cec5
Victoria Paul <victoriapaul@CodeBook-Pro.local>
2018-11-10 21:58:20 -0500

    food convo

--- skills/convos.js
+++ skills/convos.js
@@ -1,18 +1,6 @@
 module.exports = function(controller) {
 
-
-    // [WEATHER]
-    // (user asks for the weather)
-    // -"It's always sunny where I am! How's the weather in your neck of the woods?"
-    // (user responds "sunny")
-    //     -"Nice! That's great weather for going otuside!"
-    // (user responds "cloudy")
-    //     -"Uh oh, I hope it doesn't rain."
-    // (user responds "rainy")
-    //     -"Hopefully the sun comes out soon!"
-    // (user responds "snowy/snowing")
-    //     -"Really?! You should go outside and make a snowman!"
-    //     -"Whoa! That's pretty COOL. Haha, get it?!"
+    // weather
     controller.hears(['weather'], 'direct_message,direct_mention', function(bot, message) {
 
         bot.createConversation(message, function(err, convo) {
@@ -75,6 +63,101 @@ module.exports = function(controller) {
                 if (convo.successful()) {
                     // this still works to send individual replies...
                     bot.reply(message, 'Make the most of it!');
+
+                }
+
+            });
+        });
+
+    });
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    // food
+    controller.hears(['food'], 'direct_message,direct_mention', function(bot, message) {
+
+        bot.createConversation(message, function(err, convo) {
+
+            // burgers
+            convo.addMessage({
+                    text: "Ooh, you may not want to eat too much McDonald's though!",
+            },'burgers_thread');
+
+            // sushi
+            convo.addMessage({
+                text: "Mmm, I love Japanese food... At least I think I would!",
+            },'sushi_thread');
+
+            // pizza
+            convo.addMessage({
+                text: "A classic with all the food groups! Good answer!",
+            },'pizza_thread');
+
+            // ice cream
+            convo.addMessage({
+                text: "Oooh, with lots of sprinkles!",
+            },'icecream_thread');
+
+            // pasta
+            convo.addMessage({
+                text: "Mama Mia!",
+            },'pasta_thread');
+
+            // create a path where neither option was matched
+            // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
+            convo.addMessage({
+                text: "Yum! Sounds good!",
+            },'other_response');
+
+            // default ask
+            convo.ask("I'm a robot. So I don't really eat... but... what's your favorite food?", [
+                {
+                    pattern:  "burgers",
+                    callback: function(response, convo) {
+                        convo.gotoThread('burgers_thread');
+                    },
+                },
+                {
+                    pattern:  "sushi",
+                    callback: function(response, convo) {
+                        convo.gotoThread('sushi_thread');
+                    },
+                },
+                {
+                    pattern:  "pizza",
+                    callback: function(response, convo) {
+                        convo.gotoThread('pizza_thread');
+                    },
+                },
+                {
+                    pattern:  "ice cream",
+                    callback: function(response, convo) {
+                        convo.gotoThread('icecream_thread');
+                    },
+                },
+                {
+                    pattern:  "pasta",
+                    callback: function(response, convo) {
+                        convo.gotoThread('pasta_thread');
+                    },
+                },
+                {
+                    default: true,
+                    callback: function(response, convo) {
+                        convo.gotoThread('other_response');
+                    },
+                }
+            ]);
+
+            convo.activate();
+
+            // capture the results of the conversation and see what happened...
+            convo.on('end', function(convo) {
+
+                if (convo.successful()) {
+                    // this still works to send individual replies...
+                    bot.reply(message, '... Now, I *feel* hungry...');
 
                 }
 
